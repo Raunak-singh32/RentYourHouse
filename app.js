e341aee70fb5b88362b644e49bd106599463c402
Raunak Singh <2005sraunak1@gmail.com>
2026-06-03 16:22:01 +0530

    fix port for render deployment

--- app.js
+++ app.js
@@ -121,6 +121,7 @@ app.use((err, req, res, next) => {
 });
 
 // ✅ Server listen
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`server is listening to port ${PORT}`);
 });
